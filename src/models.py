822c5645300e6944b50b3bd26c1e9cae8fe72f6d
Igor Rosliakov <drunkbones87@gmail.com>
2025-12-27 19:42:57 +0900

    Create models.py

---
+++ src/models.py
@@ -0,0 +1,9 @@
+"""
+Data models for webhook payloads.
+
+This module will contain:
+- Pydantic models for incoming webhook events
+- shared data structures used across the service
+
+Currently not implemented.
+"""
